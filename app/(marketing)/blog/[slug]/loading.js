17c83c777f8cb8680a8f18b00c5987bdd2a841b2
Tullathorn Laowang <ajeelaowang2@gmail.com>
2025-06-18 12:46:04 +0700

    fix text & add text

--- app/(marketing)/blog/[slug]/loading.js
+++ app/(marketing)/blog/[slug]/loading.js
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
     <div className="flex items-center justify-center h-screen">
-      <p className="text-xl font-bold">Loadeing... </p>
+      <p className="text-xl font-bold">Loading... </p>
     </div>
   );
 }
