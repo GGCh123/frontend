40ded258f1cbee4a2b4046ebb9e1e54551d565ee
Tullathorn Laowang <ajeelaowang2@gmail.com>
2025-06-18 12:46:42 +0700

    add text

--- app/page.js
+++ app/page.js
@@ -4,6 +4,9 @@ export default function Home() {
     <h1 className="font-bold text-4xl text-blue-400">
       <center>Home Page</center>
       </h1>
+    <p className="text-center text-gray-600 mt-4">
+      <center>ตุลธร เลาว้าง 012</center>
+    </p>
     </>
   );
 }
