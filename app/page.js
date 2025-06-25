dba410131e26b74d5961f8792b825a89b575fa65
Tullathorn Laowang <ajeelaowang2@gmail.com>
2025-06-25 09:24:42 +0700

    test change code and sign out in vscode

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
       <center>Home Page</center>
       </h1>
     <p className="text-center text-gray-600 mt-4">
-      <center>ตุลธร เลาว้าง 012</center>
+      <center>Test</center>
     </p>
     </>
   );
